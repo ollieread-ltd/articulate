46b0075e0cf825ef512bc51d816bfb03dabcfbe5
Ollie <code@ollie.codes>
2024-04-09 21:10:25 +0100

    chore: Make the Mapping class generic for the class it's mapping

--- src/Metadata/Mapping.php
+++ src/Metadata/Mapping.php
@@ -2,19 +2,22 @@
 
 namespace Articulate\Metadata;
 
+/**
+ * @template MetaClass of object
+ */
 abstract class Mapping
 {
     /**
      * The class that is being mapped
      *
-     * @return class-string
+     * @return class-string<MetaClass>
      */
     abstract public function class(): string;
 
     /**
      * Map the metadata
      *
-     * @param \Articulate\Metadata\MetadataBuilder $metadata
+     * @param \Articulate\Metadata\MetadataBuilder<MetaClass> $metadata
      *
      * @return void
      */
